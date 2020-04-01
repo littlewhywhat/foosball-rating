304079860d419982fdb929de12c708c1891ee802
Roman Vaivod <romanv@salsitasoft.com>
2020-04-01 18:03:50 +0200

    gh-147: Add game Title

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -4,7 +4,6 @@ import { Route, Switch } from 'react-router-dom'
 import * as ROUTES from './const/routes'
 import { Header } from './components/header'
 import { Footer } from './components/Footer/footer'
-import { Container } from '../styles/blocks/layout'
 // pages
 import { Dashboard } from './pages/Dashboard'
 import { LeaderboardPage } from './pages/Leaderboard'
@@ -12,13 +11,13 @@ import { CreateMatchPage } from './pages/CreateMatch'
 import { Profile } from './pages/Profile'
 import { AddPlayerPage } from './pages/AddPlayer'
 import { MatchListPage } from './pages/MatchList'
-import { Button, Subtitle } from '../styles/blocks'
-import { isGameSelected } from './modules/games/games-selectors'
+import { SimpleButton, Subtitle, SimpleLink, Container } from '../styles/blocks'
+import { getSelectedGame } from './modules/games/games-selectors'
 import { connect } from 'react-redux'
 
 export class AppComponent extends Component {
   render() {
-    const { match: { url, params: { gameName } }, isGameSelected, gameNotFound } = this.props
+    const { match: { url, params: { gameName } }, selectedGame, gameNotFound } = this.props
     const createMatch = () => {
       this.props.history.push(`${url}${ROUTES.CREATE_MATCH}`)
     }
@@ -26,10 +25,15 @@ export class AppComponent extends Component {
     return (
       <>
         <Header>
-          { isGameSelected? <Button onClick={createMatch}>Add Match</Button> : null }
+          { selectedGame? <>
+            <span id='title'>
+              <SimpleLink to={url}>{selectedGame.name.toUpperCase()}</SimpleLink>
+            </span>
+            <SimpleButton onClick={createMatch}>Add Match</SimpleButton>
+          </> : null }
         </Header>
         <Container>
-          { isGameSelected
+          { selectedGame
             ? <Switch>
               <Route exact path={constructUrl(ROUTES.LEADERBOARD)}>
                 <LeaderboardPage constructUrl={constructUrl} />
@@ -56,7 +60,7 @@ export class AppComponent extends Component {
 }
 
 const mapStateToProps = state => ({
-  isGameSelected: isGameSelected(state),
+  selectedGame: getSelectedGame(state),
   gameNotFound: state.gameNotFound,
 })
 
